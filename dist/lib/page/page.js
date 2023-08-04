a92649c86a717ddc57e63aba329008bcc0b7ab9f
Hamid <hamid@alphamudigital.com>
2023-08-04 17:22:45 +0500

    Remove dist folder and add it in .gitignore

--- dist/lib/page/page.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Page = void 0;
-class Page {
-    constructor(browser) {
-        this.browser = browser;
-    }
-}
-exports.Page = Page;
--- dist/lib/utils/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sampleHelper = void 0;
-// helpers here
-const sampleHelper = () => {
-    console.log("Test");
-};
-exports.sampleHelper = sampleHelper;
